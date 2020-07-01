0d3aa17a3d37627c4517f68c7641fa2fc00b49bd
Ubuntu <ubuntu@ip-172-31-43-26.ap-south-1.compute.internal>
2020-07-01 18:15:15 +0000

    first commit

---
+++ grads.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html amp lang="en">
+<head>
+
+     <title>SpringField Olympiads</title>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="springfield olympiads private limited we, at Springfield provide best olympiad solutions for students.Springfield Olympiads is one the few establishments which was founded with the aim of not only building competitive spirit but also focusing on developing healthy academic competition among children at the school level.The status-quo of Springfield Olympiads can be attributed  to driving academicians who felt a need to promote/advance science, arithmetic, computer training, English, General Knowledge, and other expert courses.Springfield Olympiads firmly believes that such Olympiads, also help the participants in gaining confidence for competition with the best around them, within the country and internationally and contribute to temperament development. Also when children experience a definite sense of accomplishment, it encourages them to try and do even higher as they advance in age.">
+     <meta name="keywords" content="Olympiads, olympiad , maths olympiads , Science olympiads ,national olympiads, springfield olympiads, competitive olympiads,science, arithmetic, computer training, English, General Knowledge, and other expert courses  ">
+     <meta name="author" content="nagacharan">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>
+     <link rel="shortcut icon" type="image/x-icon" href="images/springfield_olympiads_private_limited_651397.png" />
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+     <link rel="stylesheet" href="css/owl.carousel.css">
+     <link rel="stylesheet" href="css/owl.theme.default.min.css">
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/bootstrap.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/base-style.css">
+     <link rel="stylesheet" href="css/olympiad.css">
+     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+     <script async src="https://cdn.ampproject.org/v0.js"></script>
+<link rel="canonical" href="http://example.ampproject.org/article-metadata.html">
+<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+<script type="application/ld+json">
+      {
+        "@context": "http://schema.org",
+        "@type": "NewsArticle",
+        "headline": "Open-source framework for publishing content",
+        "datePublished": "2015-10-07T12:02:41Z",
+        "image": [
+          "logo.jpg"
+        ]
+      }
+    </script>
+      <script>
+$(function(){
+$("#header1").load("header.php");
+$("#footer1").load("footer.php");
+});
+    </script>
+    <style type="text/css">
+        .card{
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      margin-bottom: 40px;
+      font-size: 20px;
+      margin-top: 10px;
+      /* margin-left: 50px;
+      margin-right: 50px;*/
+
+    }
+    .card-body{
+      margin-left: 25px;
+      margin-right: 25px;
+     }
+    </style>
+
+
+
+</head>
+<body id="top" data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+
+<button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fas fa-chevron-up"></i></button>
+
+     <!-- MENU -->
+
+    <div id="header1"class="mb-5"></div>
+     <br/><br/><br/><br/><br/>
+
+
+     <img src="images/hgfgj.jpg" class="img-responsive">
+
+
+
+
+
+          <div class="container-fluid" id="olympiad">
+          
+<div class="card">
+    <div class="card-body">
+        <br/>
+        
+                    <p> <i class="fas fa-graduation-cap"></i>The Grads Olympiad is one of its kind Olympiad, specially curated for graduates and undergraduates. This is an online multiple-choice question based Olympiad. </p>
+                    <br/>
+                    <p> <i class="fas fa-hand-point-right"></i><b>You should apply if you are:</b></p>
+                       <ul>
+                       <li>Preparing for your placements.</li>
+                       <li>Preparing for various B-school entrance exams like CAT, XAT, IIFT, etc.</li>
+                       <li>Keen to test your aptitude skills.</li>
+                       <li>Preparing for any other type of competitive exams like govt. exams.</li><br>
+                       </ul>
+                    <p><i class="fas fa-edit"></i><b>Registration of Students:</b> The Grads Olympiad is open to all. This Olympiad will help all enthusiasts who are preparing for one exam or the other. This Olympiad provides a win-win situation for all the candidates, that can yield fruitful results. Upon successful registration, Springfield Olympiads will register the applicant and the applicant will receive the email regarding guidelines and login credentials for the examination.
+                    <a href="https://rzp.io/l/springfield" style="font-style: normal;">Click here to register</a></p>
+<br/>
+                    <p> <i class="fas fa-book-open"></i><b>Syllabus:</b>  The Olympiad will be divided into 3 sections </p>
+                       <ul>
+                       <li>Basic Quants</li>
+                       <li>Logical Reasoning</li>
+                       <li>Verbal Ability</li>
+                       </ul>
+                    <p>  Each section will have 10 questions, summing to 30 questions.  </p>
+                    <p> The duration of Olympiad is of 1 hour only.  </p>
+                    <p><a href="winners.php">Winners</a></p>
+
+ <br/>                   
+                    
+                    <p><i class="fas fa-money-check-alt"></i><b>REGISTRATION FEE:</b>  A nominal fee of INR 99 only is charged to meet ends.</p>
+                    <br/>
+<p><i class="fas fa-award"></i><b>PRIZES: </b> </p>
+                       <ul>
+                       <li>All participants will be provided with certificates.</li>
+                       <li>The list of top 3 participants from each state will be displayed on our website. Also, top 10 participants from each state will get Merit Certificates. </li>
+                       <li>The toppers will also get cash prizes.</li>
+                       </ul>
+<br/>
+<p><i class="fas fa-money-check-alt"></i><b>Date of exam:</b>  The Olympiad window will be open from 16th May (1500  IST) to 17th May 2020(1500 IST).</p><br><br>
+<center><div class="razorpay-embed-btn" data-url="https://pages.razorpay.com/pl_EkbrT6qCecWwRx/view" data-text="Register Now" data-color="#002992" data-size="medium">
+<div>
+                        <div style="width:180px; height:40px; color:white; padding:5px; background-color:darkblue;">
+                            <b style="text-decoration:none;">Register Now</b>
+                        </div>
+                    </div>
+</div></center>
+</div></div>
+
+
+          </div>
+ <div id="footer1"></div>
+     <script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+     <script src="backtotop.js"></script>
+     <script>
+        var x = $('b').click(()=>{
+            window.location.href="https://pages.razorpay.com/sfolyG";
+        });
+        
+    </script>
+</body>
+</html>
